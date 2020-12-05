67741be5f700e1ee8123ee79b8be1c3d91abf104
mrasdfdela <jerryhsu830@gmail.com>
2020-12-05 16:34:26 -0500

    assignment 1

--- assignment_1.rb
+++ assignment_1.rb
@@ -1,1 +1,26 @@
-puts 'hello world!'
+def fibs(n)
+  arr = [0,1]
+  (n-1).times { |i| arr.push(arr[-1]+arr[-2]) }
+  arr
+end
+
+def fibs_rec(n,arr=[0,1])
+  if n == 1
+    arr
+  else
+    el = arr[-1] + arr[-2]
+    fibs_rec(n - 1, arr << el)
+  end
+  arr
+end
+
+puts
+print fibs(3)
+print fibs(4)
+print fibs(5)
+print fibs(6)
+
+puts
+print fibs_rec(3)
+print fibs_rec(4)
+print fibs_rec(5)
